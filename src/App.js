e0313d138bd0c7b2d5eac10c9021d99967987c63
jd-pavan <pavanjidimath4901@gmail.com>
2023-06-14 16:51:58 +0530

    react first push 14/06/2023

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+// import SlideBar from './components/SlideBar';
+import Loginpage from './components/loginpage';
+
+function App() {
+  return (
+    <div >
+      {/* <SlideBar /> */}
+      <Loginpage />
+    </div>
+  );
+}
+
+export default App;
