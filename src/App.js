2ddeb9687dbf98d37cb3676d9aa888abda76cd66
jd-pavan <pavanjidimath4901@gmail.com>
2023-09-11 18:52:57 +0530

    Update Code(sidebar remaining) 11/09/2023

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Routes,
@@ -9,6 +9,21 @@ import Adminhome from './Pages/AdminPgs/Adminhome';
 import ClientPages from './Pages/ClientMobilePgs/ClientPages';
 
 function App() {
+
+
+  useEffect(() => {
+    // Add an event listener to clear local storage when the user leaves the page
+    window.addEventListener('beforeunload', () => {
+      localStorage.clear();
+    });
+
+    // Remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('beforeunload', () => {
+        localStorage.clear();
+      });
+    };
+  }, []);
   return (
 
     <div>
